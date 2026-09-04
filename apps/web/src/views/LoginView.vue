a97507bf0c3ccb9f90c20e229d4b6d3e204e1698
羁木轮 <1844622298@qq.com>
2026-09-04 22:50:11 +0800

    style(apps/web): 登录页 username 图标换成用户图标（Tabler user，保持复刻版式）

--- apps/web/src/views/LoginView.vue
+++ apps/web/src/views/LoginView.vue
@@ -8,6 +8,7 @@
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
+import IconUser from '~icons/tabler/user'
 import { useAuthStore } from '@/stores/auth'
 import '@/styles/mobile-soft.css'
 
@@ -58,13 +59,7 @@ function notReady(text: string) {
   <div class="s-login">
     <form class="form" @submit.prevent="submit">
       <div class="inputForm">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 32 32" height="20">
-          <g id="Layer_3" data-name="Layer 3">
-            <path
-              d="m30.853 13.87a15 15 0 0 0 -29.729 4.082 15.1 15.1 0 0 0 12.876 12.918 15.6 15.6 0 0 0 2.016.13 14.85 14.85 0 0 0 7.715-2.145 1 1 0 1 0 -1.031-1.711 13.007 13.007 0 1 1 5.458-6.529 2.149 2.149 0 0 1 -4.158-.759v-10.856a1 1 0 0 0 -2 0v1.726a8 8 0 1 0 .2 10.325 4.135 4.135 0 0 0 7.83.274 15.2 15.2 0 0 0 .823-7.455zm-14.853 8.13a6 6 0 1 1 6-6 6.006 6.006 0 0 1 -6 6z"
-            />
-          </g>
-        </svg>
+        <IconUser aria-hidden="true" />
         <input
           id="vv-email"
           v-model="email"
@@ -220,6 +215,13 @@ function notReady(text: string) {
   padding-left: 10px;
   transition: border 0.2s ease-in-out;
   min-width: 0;
+}
+
+.inputForm svg {
+  width: 20px;
+  height: 20px;
+  color: #151717;
+  flex: 0 0 auto;
 }
 
 .inputForm:focus-within {
